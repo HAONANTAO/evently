4dff49a85ec6a373a92bb7c59c728e9ca1addc4a
HAONANTAO <873190199@qq.com>
2024-05-05 22:51:30 +1000

    user created webhook

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,18 @@
 'use server'
 import { CreateUserParams } from "@/types"
+import { handleError } from "../utils"
+import { connectToDatabase } from "../database"
+import User from "../database/models/user.model"
 //backend logic
 export const createUser = async(user:CreateUserParams)=>{
     try {
-      
+      // serverless function
+      await connectToDatabase();
+
+      const newUser = await User.create(user);
+      // keep pure
+      return JSON.parse(JSON.stringify(newUser));
     } catch (error) {
-      
+      handleError(error)
     }
 }
