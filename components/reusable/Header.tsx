76c9ec8437c9a4ceaac79d49f2757941272294f3
HAONANTAO <873190199@qq.com>
2024-05-05 19:42:28 +1000

    add shadcn sheet

--- components/reusable/Header.tsx
+++ components/reusable/Header.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
-import { SignedOut } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import { MobileNav } from "./MobileNav";
 const Header = () => {
   return (
     <header className="w-full border-b">
@@ -15,7 +17,18 @@ const Header = () => {
             alt="evently logo"
           />
         </Link>
+        {/* show when large devices */}
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+        {/* if signed out */}
         <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
               <Link href="/sign-in">Login</Link>
