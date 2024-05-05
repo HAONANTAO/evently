e031be6511c5bf313098f2128c19b5c2c4803550
HAONANTAO <873190199@qq.com>
2024-05-05 22:07:09 +1000

    using github copilot add interface

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,6 +1,21 @@
 import { url } from "inspector";
 import { model, models, Schema } from "mongoose";
 
+export interface IEvent extends Document{
+  _id: string,
+  title: string,
+  description: string,
+  location: Location,
+  createdAt: Date,
+  imageUrl: string,
+  startDateTime: Date,
+  endDateTime: Date,
+  price: number,
+  isFree: boolean,
+  url: string,
+  category: { _id: string, name: string },
+  organizer: { _id: string, firstName: string, lastName: string }
+}
 const EventSchema = new Schema({
   title:{type:String, required:true},
   description:{type:String},
