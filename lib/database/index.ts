dfce9a5338b27f78103ee90568a97d7653c3f8bb
HAONANTAO <873190199@qq.com>
2024-05-06 20:45:07 +1000

    change db

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,23 +1,25 @@
- import mongoose from "mongoose"
-
- const MONGODB_URL = process.env.MONGODB_URL
-// in serverless function need to reduce the many connection 
+import mongoose from "mongoose";
+import "dotenv";
+const MONGODB_URL = process.env.MONGODB_URL;
+// in serverless function need to reduce the many connection
 //  初始化缓存，使用global避开严格ts检查
- let cached = (global as any).mongoose || {conn:null,promise:null}
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
- export const connectToDatabase = async()=>{
+export const connectToDatabase = async () => {
   // 有就直接拿
-  if(cached.conn) return cached.conn;
+  if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URL) throw new Error("MONGODB_URL is missing!")
+  if (!MONGODB_URL) throw new Error("MONGODB_URL is missing!");
 
-    // 给一个链接db的promise任务
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL,{
-      dbName:"evently",
+  // 给一个链接db的promise任务
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "evently",
       // 这通常在数据库连接不稳定或者在启动时数据库尚未准备好时有用，以避免在连接实际建立之前缓存太多命令。
-      bufferCommands:false,
-    })
-    // waiting connection
-    cached.conn = await cached.promise;
-    return cached.conn
- }
+      bufferCommands: false,
+    });
+  // waiting connection
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,15 +1,15 @@
 import { model, models, Schema } from "mongoose";
 
 const UserSchema = new Schema({
-  clerkId:{type: String,required: true,unique:true},
-  email:{type: String,required:true,unique:true},
-  username:{type: String,required:true,unique:true},
-  firstName:{type: String,required:true},
-  lastName:{type: String,required:true},
-  photo:{type: String,required:true},
-})
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  firstName: { type: String },
+  lastName: { type: String },
+  photo: { type: String, required: true },
+});
 
 // check first
-const User = models.User || model("User",UserSchema)
+const User = models.User || model("User", UserSchema);
 
-export default User
+export default User;
