51406560401f66fc5b1962fd6e340bc563b45e28
HAONANTAO <873190199@qq.com>
2024-05-07 19:48:05 +1000

    building the event form of creation

--- components/reusable/Dropdown.tsx
+++ components/reusable/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { startTransition, useState } from "react";
 import {
   Select,
   SelectContent,
@@ -7,6 +7,19 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { ICategory } from "../../lib/database/models/category.model";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Input } from "../ui/input";
+
 type DropdownProps = {
   onChangeHandler?: () => void;
   value?: string;
@@ -14,10 +27,10 @@ type DropdownProps = {
 
 const Dropdown = ({ onChangeHandler, value }: DropdownProps) => {
   // dynamic set of my category
-  const [categories, setCategories] = useState<ICategory[]>([
-    { _id: "1", name: "Category 1" },
-    { _id: "2", name: "Category 2" },
-  ]);
+  const [categories, setCategories] = useState<ICategory[]>([]);
+  const [newCategory, setNewCategory] = useState("");
+  const handleAddCategory = () => {};
+
   return (
     <Select onValueChange={onChangeHandler} defaultValue={value}>
       {/* style the field */}
@@ -31,9 +44,36 @@ const Dropdown = ({ onChangeHandler, value }: DropdownProps) => {
               key={category._id}
               value={category._id}
               className="select-item p-regular-14">
-             { category.name}
+              {category.name}
             </SelectItem>
           ))}
+        <AlertDialog>
+          <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500 ">
+            Open
+          </AlertDialogTrigger>
+          <AlertDialogContent className="bg-white">
+            <AlertDialogHeader>
+              <AlertDialogTitle>New Category</AlertDialogTitle>
+              <AlertDialogDescription>
+                <Input
+                  type="text"
+                  placeholder="Category Name"
+                  className="input-field mt-3"
+                
+                  onChange={(e) => setNewCategory(e.target.value)}
+                />
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction
+                // 非重要更新
+                onClick={() => startTransition(handleAddCategory)}>
+                Add
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </SelectContent>
     </Select>
   );
--- components/reusable/EventForm.tsx
+++ components/reusable/EventForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -17,6 +17,8 @@ import { Input } from "@/components/ui/input";
 import { eventFormSchema } from "../../lib/validator";
 import { eventDefaultValues } from "@/data";
 import Dropdown from "./Dropdown";
+import { Textarea } from "../ui/textarea";
+import { FileUploader } from "./FileUploader";
 interface EventFormProps {
   userId: string;
   type: "Create" | "Update";
@@ -26,6 +28,7 @@ interface EventFormProps {
 //   type: "Create" | " Update";
 // };
 const EventForm = ({ userId, type }: EventFormProps) => {
+  const [files, setFiles] = useState<File[]>([]);
   // const initialValues =eventDefaultValues;
   // 1. Define your form.
   const form = useForm<z.infer<typeof eventFormSchema>>({
@@ -78,8 +81,45 @@ const EventForm = ({ userId, type }: EventFormProps) => {
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
         </div>
+        <div className="flex flex-col gap-5 md:flex-row">
+          {" "}
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Title</FormLabel>
+                <FormControl className="h-72">
+                  <Textarea
+                    placeholder="Description"
+                    className="textarea round-2xl"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Title</FormLabel>
+                <FormControl className="h-72">
+                  <FileUploader
+                    onFieldChange={field.onChange}
+                    imageUrl={field.value}
+                    setFiles={setFiles}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button type="submit">Submit</Button>
       </form>
     </Form>
   );
