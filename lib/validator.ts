63ee6bc4b851e696d11da59499c4cc6b9e4ac0f5
HAONANTAO <873190199@qq.com>
2024-05-09 21:28:31 +1000

    update event part works

--- lib/validator.ts
+++ lib/validator.ts
@@ -7,7 +7,7 @@ export const eventFormSchema = z.object({
     .string()
     .min(3, "Description must be at least 3 characters.")
     .max(400, "Description must be at most 400 characters."),
-  location: z.string().min(3, "Location must be at least 3 characters.").max(400,"Location must be at most 400 characters."),
+  location: z.string().min(3, "Location must be at least 3 characters.").max(800,"Location must be at most 800 characters."),
   imageUrl:z.string(),
   startDateTime:z.date(),
   endDateTime:z.date(),
