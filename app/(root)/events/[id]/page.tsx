5457fb651e425a7a654c9056ca435acc2de80bd8
HAONANTAO <873190199@qq.com>
2024-05-11 00:18:11 +1000

    finish
    finish

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -82,9 +82,10 @@ const EventDetails = async({params:{id},searchParams}:SearchParamProps) => {
             emptyTitle="No Events Found"
             emptyStateSubtext="Come back later"
             collectionType="All_Events"
-            limit={6}
-            page={1}
-            totalPages={2}
+            limit={3}
+            // assertion as string 
+            page={searchParams.page as string}
+            totalPages={relatedEvents?.totalPages}
           />
     </section>
     </>
