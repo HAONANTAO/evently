2fc25d0d76982c3b48370ff39d5c587aab62009d
HAONANTAO <873190199@qq.com>
2024-05-10 12:50:51 +1000

    testing the endpoint of stripe

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,7 +1,10 @@
 "use server"
 
-import { CheckoutOrderParams } from "@/types"
+import { CheckoutOrderParams, CreateOrderParams } from "@/types"
 import { redirect } from "next/navigation";
+import { handleError } from "../utils";
+import { connectToDatabase } from "../database";
+import Order from "../database/models/order.model";
 // import Stripe from 'stripe';
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
   const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
@@ -39,3 +42,16 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
   }
 }
 
+export const createOrder = async(order: CreateOrderParams) => {
+  try {
+    await connectToDatabase();
+    const newOrder = await Order.create({
+      ...order,
+      event:order.eventId,
+      buyer:order.buyerId
+    });
+    return JSON.parse(JSON.stringify(newOrder))
+  } catch (error) {
+    handleError(error)
+  }
+}
